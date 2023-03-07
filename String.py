d55ade7cc6b54b8e189f444863ef5dd0ff81e4f5
vinayakmishar4 <88668137+vinayakmishar4@users.noreply.github.com>
2023-03-07 17:41:14 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,14 @@
+# String is enclosed with double and single quotes to consdiered as string datatype.
+#  String is array of characters of textual data.
+name=input("Enter your name:- ")
+print("Your name is :- ",name)
+# Multiple String:-
+# String is which are quotes triplet qutoes to consdiered as string datatype
+fruit='''Apple is a fruit , it was tasty to eat and it is jucie is very tasty to drink'''
+print("Tell me about one  fruit : " , fruit)
+# In python string is array of characters
+# Which is strated from index level zero
+print("Length of a name = " , len(name))
+print("Charcters in the name :-")
+for charcter in name:
+    print(charcter)
---
+++ StringOperations.py
@@ -0,0 +1,8 @@
+# various String Operations
+word=input("Enter the word:- ")
+start=int(input("Start of String:- "))
+end=int(input("Start of String:- "))
+# String Slicing is obtained by substring of a string it contains start and end.
+print(word[start:end])
+# length of the string
+print("Length of String = ",len(word))
