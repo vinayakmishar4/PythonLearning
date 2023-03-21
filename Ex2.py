888a339636b04cce7f242257b3df44fb672a09aa
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2023-03-21 17:30:23 +0530

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,10 @@
+# WAP to get current time
+import time
+Time=time.strftime('%H:%M:%S')
+print("Current time is this", Time)
+Hour=time.strftime('%H')
+print('Current hour is this', Hour)
+Min=time.strftime('%M')
+print('Current minute is this', Min)
+Sec=time.strftime('%S')
+print('Current second is this',Sec)
