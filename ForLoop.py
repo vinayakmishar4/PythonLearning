c0ff970089f06b020cef18e092e7ce87f76d6115
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2023-04-01 20:27:54 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,14 @@
+# for loop  in python
+# to iterate over a sequence of objects
+# Strings
+name=input("Enter The Name:- ");
+print("Letter of name: " )
+for i in name:
+    print(i,end=",")
+# List
+colours=["Red", "Green", "Yellow", "Blue"]
+print("\nColours Name: ")
+for i in colours:
+    print(i)
+
+
---
+++ whileLoops.py
@@ -0,0 +1,15 @@
+# While loops in python 
+num=int(input("Enter a number :- "))
+print("This is Increament While loop")
+while (num<20):
+    # print number num to 5
+    print(num)
+    # it will increment add 1
+    num=num+1
+print("This is Decreament Loop")
+num2=-5
+while (num2>0):
+    # print number num to 20
+    print(num2)
+    # it will increment subttract 1
+    num2=num2-1
