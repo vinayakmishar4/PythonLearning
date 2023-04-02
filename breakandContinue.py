8dfff7c52e396cd5108242949258120a2597cc79
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2023-04-02 16:54:15 +0530

    Add files via upload

---
+++ breakandContinue.py
@@ -0,0 +1,9 @@
+# break and continue in python 
+# break :- it is a statment to skip the part of code
+for i in range(20):
+    print(i,"Hello")
+    break
+#continue :- it used to skip iteration 
+for i in range(20):
+    print(i,"Hello")
+    continue
